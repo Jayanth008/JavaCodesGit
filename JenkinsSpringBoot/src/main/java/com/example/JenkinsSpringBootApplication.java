74c8de677c49ed75c6b2b46f8e588c4a2236c48b
BUDDI SHIVA PRASAD <buddhishivaprasad7@gmail.com>
2024-01-11 15:11:46 +0530

    JenkinsSpringBootFirstCommit

---
+++ JenkinsSpringBoot/src/main/java/com/example/JenkinsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringBootApplication.class, args);
+	}
+
+}
