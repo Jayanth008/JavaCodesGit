bfc81cbdcb7406b6a5a62653eb496ffb60063899
BUDDI SHIVA PRASAD <buddhishivaprasad7@gmail.com>
2024-01-12 11:11:27 +0530

    second commit

--- JenkinsSpringBoot/src/test/java/com/example/JenkinsSpringBootApplicationTests.java
+++ JenkinsSpringBoot/src/test/java/com/example/JenkinsSpringBootApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsSpringBootApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(JenkinsSpringBootApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test Case Executing...");
+
+		assertEquals(true, true);
+		logger.info("Text Completed....");
 	}
 
 }
