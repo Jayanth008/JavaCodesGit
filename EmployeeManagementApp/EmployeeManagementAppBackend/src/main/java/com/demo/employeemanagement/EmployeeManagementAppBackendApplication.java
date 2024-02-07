f7e04c39f6b8a3fc03b35f44fcef082d9c892cef
BUDDI SHIVA PRASAD <buddhishivaprasad7@gmail.com>
2024-02-07 18:46:27 +0530

    first commit

---
+++ EmployeeManagementApp/EmployeeManagementAppBackend/src/main/java/com/demo/employeemanagement/EmployeeManagementAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementAppBackendApplication.class, args);
+	}
+
+}
---
+++ EmployeeManagementApp/EmployeeManagementAppBackend/src/test/java/com/demo/employeemanagement/EmployeeManagementAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
